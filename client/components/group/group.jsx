dfca5e25dbc5d36ddc23d596076d1a1574d2f152
ramdayalmunda <r.dayal.munda@gmail.com>
2024-03-17 13:43:54 +0530

    modal for group image

--- client/components/group/group.jsx
+++ client/components/group/group.jsx
@@ -1,4 +1,4 @@
-import { Text, ScrollView, TouchableOpacity, View, SafeAreaView, Pressable, Image, StyleSheet } from "react-native"
+import { Text, ScrollView, TouchableOpacity, View, SafeAreaView, Pressable, Image, StyleSheet, Modal, Dimensions } from "react-native"
 import { useRouter, Stack } from "expo-router"
 import { useEffect, useState } from "react"
 import api from "../../common/api"
@@ -9,6 +9,12 @@ export default function ({ loginhandler }) {
     let router = useRouter()
 
     var [groupList, setGroupList] = useState([])
+    const [modalVisible, setModalVisible] = useState(false);
+    const [ modalData, setModalData ] = useState({
+        imageName: "",
+        imageUrl: "",
+    })
+
     function navigateTo(path) {
         router.push(path)
     }
@@ -30,6 +36,15 @@ export default function ({ loginhandler }) {
         await logout()
         await loginhandler()
     }
+
+    const setupModal = (data)=>{
+        setModalData({ imageTitle: data.name, imageUrl: data.imageUrl })
+        setModalVisible(true)
+    }
+
+    const closeModal = () => {
+        setModalVisible(false);
+    };
 
     useEffect(() => {
         getGroupList()
@@ -55,28 +70,56 @@ export default function ({ loginhandler }) {
                         style={styles.groupItem}
                         onPress={() => { navigateTo("/chat") }}
                     >
-                        <Image
-                            source={{ uri: `http://192.168.105.212:3081/images/${item.imageUrl}` }}
-                            style={ styles.image }
-                        />
-                        <Text
-                            style={{
-                                fontSize: 15,
-                            }}
-                        >{item.name}</Text>
+                        <View style={{ flexDirection: 'row', alignItems: 'center'}}>
+                            
+                            <TouchableOpacity onPress={()=>{ setupModal(item) }} >
+                                <Image
+                                    source={{ uri: `http://192.168.105.212:3081/images/${item.imageUrl}` }}
+                                    style={styles.image}
+                                />
+                            </TouchableOpacity>
+                            <Text
+                                style={{
+                                    fontSize: 15,
+                                    paddingLeft: 15,
+                                }}
+                            >{item.name}</Text>
+                        </View>
+                        <Text>6</Text>
                     </TouchableOpacity>
                 ))}
+
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={closeModal}
+                >
+
+                    <View style={styles.modalContainer}>
+                        <View style={styles.modalContent}>
+                            <View style={styles.modalTitle} >
+                                <Text>{modalData.imageTitle}</Text>
+                                <TouchableOpacity onPress={closeModal} style={styles.closeButton}>
+                                    <Text style={{textAlign: 'center', marginTop: 2 }}>X</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View>
+                                <Image
+                                    source={{ uri: `http://192.168.105.212:3081/images/${modalData.imageUrl}` }}
+                                    style={styles.modalImage}
+                                />
+                            </View>
+                        </View>
+                    </View>
+
+                </Modal>
             </SafeAreaView>
         </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
     image: {
         width: 50,
         height: 50,
@@ -90,6 +133,39 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         paddingHorizontal: 16,
+        justifyContent: 'space-between',
         paddingVertical: 8,
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
+    },
+    modalContent: {
+        width: Dimensions.get('window').width-30,
+        // height: Dimensions.get('window').height-30,
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 10,
+        elevation: 5,
+    },
+    modalTitle: {
+        flexDirection: 'row',
+        verticalAlign: 'middle',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+    },
+    modalImage: {
+        width: '100%',
+        resizeMode: 'contain',
+        minHeight: Dimensions.get('window').width,
+    },
+    closeButton: {
+        color: 'blue',
+        backgroundColor: '#789',
+        width: 24,
+        height: 24,
+        borderRadius: 5,
     },
 });
