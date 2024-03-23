44c9b6183d796f47ea41fae1bc30532c9e1930c5
ramdayalmunda <r.dayal.munda@gmail.com>
2024-03-24 00:11:09 +0530

    api middleware removed

--- server/index.js
+++ server/index.js
@@ -11,11 +11,6 @@ app.use( (req, res, next)=>{
     next()
 } )
 
-app.use( "/api", (req, res)=>{
-    console.log('caching api')
-    res.status(401).json({ message: "test 401" })
-} )
-
 require("./controller/socket.js")
 
 app.use(express.json())
