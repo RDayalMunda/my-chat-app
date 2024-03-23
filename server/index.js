f4604c7aa4343e56e74df723d5c5200f8dc43326
ramdayalmunda <r.dayal.munda@gmail.com>
2024-03-24 00:03:19 +0530

    server comment

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 
+app.use( (req, res, next)=>{
+    console.log('server reached')
+    next()
+} )
+
 require("./controller/socket.js")
 
 app.use(express.json())
